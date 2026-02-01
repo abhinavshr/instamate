60c55d53c67397250c48a17f4845a44611244a39
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-02-01 18:18:37 +0545

    feat: added the dynamic feature for the register

---
+++ lib/api/auth_api.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AuthApi {
+  static const String _baseUrl = 'http://10.0.2.2:5000/api/auth';
+
+  static Future<http.Response> register(Map<String, dynamic> data) {
+    return http.post(
+      Uri.parse('$_baseUrl/register'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(data),
+    );
+  }
+}
