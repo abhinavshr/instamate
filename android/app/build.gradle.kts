d544cfbdf2710390f2e1a3329a9ec82e988e1651
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-02-06 22:22:51 +0545

    feat: added the static profile update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.instamate"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
